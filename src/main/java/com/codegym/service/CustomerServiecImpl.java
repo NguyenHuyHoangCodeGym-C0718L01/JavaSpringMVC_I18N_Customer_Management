8cf318234735edd075e56f595fbcba8fd62e4b68
Nguyen Huy Hoang <huyhoang681997@gmail.com>
2019-04-18 09:28:38 +0700

    Hoang finish Java Spring MVC I18N Customer Management project.

---
+++ src/main/java/com/codegym/service/CustomerServiecImpl.java
@@ -0,0 +1,40 @@
+package com.codegym.service;
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import com.codegym.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CustomerServiecImpl implements CustomerService{
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public Iterable<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findOne(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.delete(id);
+    }
+
+    @Override
+    public Iterable<Customer> findAllByProvince(Province province) {
+        return customerRepository.findAllByProvince(province);
+    }
+}
